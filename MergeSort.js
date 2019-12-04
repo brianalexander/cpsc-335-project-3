33b6d8e3505b7104d469e8198f1319af1e4982f7
Brian Alexander <brianalexander28@gmail.com>
2019-12-03 17:31:09 -0800

    added stacks to MergeSort

--- MergeSort.js
+++ MergeSort.js
@@ -3,8 +3,11 @@ class MergeSort {
     // implement state as a stack to keep track of history
     this.state = [toSort];
 
-    // possible actions: split, compare, merge, swap, finished
-    this.nextAction = "split";
+    this.mergeStack = [];
+    this.mergeSortStack = [toSort];
+
+    // possible actions: mergeSort, compare, merge, swap, finished
+    this.nextAction = ["mergeSort"];
   }
 
   /**
@@ -22,10 +25,50 @@ class MergeSort {
         return 0;
       case "compare":
         return 1;
-      case "merge":
+      case "mergeSort":
+        this.mergeSort(this.mergeSortStack.pop());
         return 1;
-      case "swap":
+      case "merge":
+        const leftAndRight = this.mergeStack.pop();
+        this.merge(leftAndRight[0], leftAndRight[1]);
         return 2;
     }
+  }
+
+  mergeSort(arr) {
+    if (arr.length < 2) {
+      // ready to merge
+      // return arr;
+      this.mergeStack(arr);
+      this.nextAction = "merge";
+    }
+
+    // find middle value
+    const middle = Math.floor(arr.length / 2);
+
+    // left is zero to middle value, exclusive
+    const left = arr.slice(0, middle);
+
+    // right is middle value to end
+    const right = arr.slice(middle);
+
+    // call mergeSort on each half, then merge the results
+    // return merge(mergeSort(left), mergeSort(right));
+    this.mergeSortStack.push(left);
+    this.mergeSortStack.push(right);
+    this.mergeStack.push([left, right]);
+  }
+
+  merge(left, right) {
+    let arr = [];
+
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        arr.push(left.shift());
+      } else {
+        arr.push(right.shift());
+      }
+    }
+    return arr.concat(left.slice().concat(right.slice()));
   }
 }
