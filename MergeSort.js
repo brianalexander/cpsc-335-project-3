3f16a06c2dd797a7027f247311fdaa589d5874fb
Brian Alexander <brianalexander28@gmail.com>
2019-12-10 18:44:24 -0800

    update state tracking and null overwrite

--- MergeSort.js
+++ MergeSort.js
@@ -1,17 +1,12 @@
 class MergeSort {
   constructor(toSort) {
     // implement state as a stack to keep track of history
-    this.queue = toSort.map(item => [item]);
+    this.queue = toSort.map((item, index) => [[index, 1], [item]]);
+    this.arr = toSort;
 
     this.array = toSort;
-    this.currentSegmentBeginIndex = 0;
-    this.currentSegmentLength = 1;
 
-    this.array1 = this.queue.shift();
-    this.array2 = this.queue.shift();
-    this.i = 0;
-    this.j = 0;
-    this.workingArray = [];
+    this.firstRun = true;
   }
 
   /**
@@ -26,28 +21,61 @@ class MergeSort {
   step() {
     // If we have finished parsing each array segment,
     // get two more segments and continue.
-    if (this.i >= this.array1.length && this.j >= this.array2.length) {
-      this.queue.push(this.workingArray);
-      // One item in Queue.  Return 0, finished.
-      if (this.queue.length == 1) {
-        return 0;
+    if (
+      this.firstRun ||
+      (this.i >= this.array1.length && this.j >= this.array2.length)
+    ) {
+      if (!this.firstRun) {
+        this.queue.push([
+          [
+            Math.min(this.startIndex1, this.startIndex2),
+            this.length1 + this.length2
+          ],
+          this.workingArray
+        ]);
+        // One item in Queue.  Return 0, finished.
+        if (this.queue.length == 1) {
+          return 0;
+        }
       }
 
-      this.currentSegmentBeginIndex =
-        this.currentSegmentBeginIndex + this.currentSegmentLength;
+      this.firstRun = false;
 
       this.i = 0;
       this.j = 0;
-      this.array1 = this.queue.shift();
-      this.array2 = this.queue.shift();
       this.workingArray = [];
 
-      this.currentSegmentLength = this.array1.length = this.array2.length;
+      this.tuple1 = this.queue.shift();
+      this.tuple2 = this.queue.shift();
+
+      this.array1 = this.tuple1[1];
+      this.array2 = this.tuple2[1];
+
+      this.startIndex1 = this.tuple1[0][0];
+      this.length1 = this.tuple1[0][1];
+      this.endIndex1 = this.startIndex1 + this.length1;
+      this.startIndex2 = this.tuple2[0][0];
+      this.length2 = this.tuple2[0][1];
+      this.endIndex2 = this.startIndex2 + this.length2;
+
+      this.arrWorkingIndex = Math.min(this.startIndex1, this.startIndex2);
+
+      // Null out working values of this.arr
+      for (let i = this.startIndex1; i < this.endIndex1; i++) {
+        this.arr[i] = null;
+      }
+      for (let i = this.startIndex2; i < this.endIndex2; i++) {
+        this.arr[i] = null;
+      }
+
+      return 3;
     }
 
     // i is finished, add the next j
     if (this.i >= this.array1.length) {
       this.workingArray.push(this.array2[this.j]);
+      this.arr[this.arrWorkingIndex] = this.array2[this.j];
+      this.arrWorkingIndex++;
       this.j++;
       return 2;
     }
@@ -55,6 +83,8 @@ class MergeSort {
     // j is finished, add the next i
     if (this.j >= this.array2.length) {
       this.workingArray.push(this.array1[this.i]);
+      this.arr[this.arrWorkingIndex] = this.array1[this.i];
+      this.arrWorkingIndex++;
       this.i++;
       return 2;
     }
@@ -62,90 +92,16 @@ class MergeSort {
     // compare & merge the next item of each array segment
     if (this.array1[this.i] < this.array2[this.j]) {
       this.workingArray.push(this.array1[this.i]);
+      this.arr[this.arrWorkingIndex] = this.array1[this.i];
+      this.arrWorkingIndex++;
       this.i++;
       return 2;
     } else {
       this.workingArray.push(this.array2[this.j]);
+      this.arr[this.arrWorkingIndex] = this.array2[this.j];
+      this.arrWorkingIndex++;
       this.j++;
       return 2;
     }
   }
 }
-
-// class MergeSort {
-//   constructor(toSort) {
-//     this.array = toSort;
-//     this.queue = [];
-
-//     // implement queue as a stack to keep track of history
-//     for (let i = 0; i < toSort.length; i++) {
-//       this.queue.push([i, i]);
-//     }
-//     this.i = 0;
-//     this.j = 1;
-//   }
-
-//   /**
-//    * @function
-//    * @name step
-//    * @description Performs one step of the algorithm.
-//    * @memberof MergeSort
-//    * @returns {number} - Indicates the queue of the
-//    *      model, either 0->finished, 1->continue,
-//    *      2->updateGUI
-//    */
-//   step() {
-//     // No items in Queue.  Return 0, finished.
-
-//     // If we have finished parsing each array segment,
-//     // get two more segments and continue.
-//     if (this.i > this.queue[0][1] && this.j > this.queue[1][1]) {
-//       if (this.queue[0][0] < this.queue[1][0]) {
-//         this.queue.push([this.queue[0][0], this.queue[1][1]]);
-//       } else {
-//         this.queue.push([this.queue[1][0], this.queue[0][1]]);
-//       }
-
-//       this.queue.shift();
-//       this.queue.shift();
-
-//       if (this.queue.length == 1) {
-//         return 0;
-//       }
-
-//       this.i = this.queue[0][0];
-//       this.j = this.queue[1][0];
-//     }
-
-//     // // i is finished, add the remaining j's
-//     // if (this.i > this.array1[1]) {
-//     //   for (; this.j <= this.array2[1]; this.j++) {
-//     //     this.workingArray.push(this.array2[this.j]);
-//     //   }
-//     //   return 2;
-//     // }
-
-//     // // j is finished, add the remaining i's
-//     // if (this.j > this.array2[1]) {
-//     //   for (; this.i <= this.array1[1]; this.i++) {
-//     //     this.workingArray.push(this.array1[this.i]);
-//     //   }
-//     //   return 2;
-//     // }
-
-//     // compare the next item of each array segment
-//     if (this.array[this.i] > this.array[this.j]) {
-//       console.log("swap", this.array[this.i], this.array[this.j]);
-//       let temp = this.array[this.i];
-//       this.array[this.i] = this.array[this.j];
-//       this.array[this.j] = temp;
-//       this.i++;
-//       this.j++;
-//       return 2;
-//     } else {
-//       this.i++;
-//       this.j++;
-//       return 1;
-//     }
-//   }
-// }
