3c0de0fc1e7bbe07cf54023cf84816c72955a99e
migz333 <mgutierrez95@pasadena.edu>
2019-12-10 19:46:03 -0800

    v2.0

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,24 +1,66 @@
 class SelectionSort {
   constructor(toSort) {
-    // implement state as a stack to keep track of history
-    this.state = [toSort];
+    this.arr = toSort;
+    this.index = 0;
+    this.min = 0;
+    this.length = toSort.length;
+    this.current = 1;
 
-    // possible actions: compare, swap, finished
-    this.nextAction = "compare";
   }
 
   step() {
-    switch (this.nextAction) {
-      case "finished":
-        return 0;
-      case "compare":
-        return 1;
-      case "swap":
-        return 2;
+    //checks if gone beyond bounds i.e done
+    if (this.index >= this.length){
+      //pass through entire array if done and return 0
+      return 0;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    }
+    //sort
+    else {
+
+      if (this.current < this.length){
+        if (this.arr[this.current] < this.arr[this.min]){
+          // update position of minimum value atm
+          this.min  = this.current;
+          //increment current position for more comparisons
+          this.current++;
+          //compare
+          return 1;
+        }
+        if (this.arr[this.current] >= this.arr[this.min]) {
+          this.current++;
+          return 1;
+        }
+      }
+      else {
+        swap(this.arr, this.index, this.min );
+        this.index++;
+        this.min = this.index;
+        this.current = this.index+1;
+      }
+
+
+
+      
+
+      // compare 
+   
+      // if the index element is bigger than the minimum value in the array we need to swap because index should be the smallest
+      
+    }
+    
+    function swap(arr, firstIndex, secondIndex){
+      var temp = arr[firstIndex];
+      arr[firstIndex] = arr[secondIndex];
+      arr[secondIndex] = temp;
     }
   }
 }
-function swap(arr, firstIndex, secondIndex){
+
+
+
+
+
+/*function swap(arr, firstIndex, secondIndex){
   var temp = arr[firstIndex];
   arr[firstIndex] = arr[secondIndex];
   arr[secondIndex] = temp;
@@ -37,7 +79,6 @@ function selection_sort(arr) {
             min = j;
         }
     }
-
     //if the minimum isn't in the position, swap it
     if (i != min){
         swap(arr, i, min);
@@ -45,11 +86,5 @@ function selection_sort(arr) {
   }
   return arr;
 }
+*/
 
-function swap(arr, firstIndex, secondIndex){
-  var temp = arr[firstIndex];
-  arr[firstIndex] = arr[secondIndex];
-  arr[secondIndex] = temp;
-}
-
-console.log(selection_sort([ 19, 9, 8, 7, 6, 5, 6, 6, 4,3,5,2 ]))
